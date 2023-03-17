9ac597492c24afb370f670473b5ca14d7afe7f6c
Jefrie69 <86434601+Jefrie69@users.noreply.github.com>
2023-03-17 16:40:25 +0700

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,7 +1,25 @@
 const fs = require('fs')
 const chalk = require('chalk')
 // EDIT DISINI
-global.owner = ['6281220670449'] // no own
+global.owner = ['const fs = require('fs')
+const chalk = require('chalk')
+// EDIT DISINI
+global.owner = ['6281332283254'] // no own
+global.author = 'AYLIZE BOTZ' // nama author 
+global.packname = 'aylize bot whatsapp' // nama pack sticker
+global.namabot = 'AYLIZE BOTZ' // nama bot mu
+global.group = 'https://watashianime.my.id' // grup mu
+global.pic = 'https://telegra.ph/file/3d3e73c4cb0682671f25a.jpg' // logo bot
+
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.yellowBright(`Update File Terbaru ${__filename}`))
+delete require.cache[file]
+require(file)
+})'] // no own
 global.author = 'KurrXd' // nama author 
 global.packname = 'xd bot whatsapp' // nama pack sticker
 global.namabot = 'XD BOTZ' // nama bot mu
