3c18ca2cd86f0f3c9545f0cb05ccfb6e392100b8
Jefrie69 <86434601+Jefrie69@users.noreply.github.com>
2023-03-17 16:53:52 +0700

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -2,11 +2,11 @@ const fs = require('fs')
 const chalk = require('chalk')
 // EDIT DISINI
 global.owner = ['6281220670449'] // no own
-global.author = 'KurrXd' // nama author 
-global.packname = 'xd bot whatsapp' // nama pack sticker
-global.namabot = 'XD BOTZ' // nama bot mu
-global.group = 'https://bit.ly/3ZI51Qk' // grup mu
-global.pic = 'https://telegra.ph/file/b96926809d93d9f045777.jpg' // logo bot
+global.author = 'AYLIZE BOTZ' // nama author 
+global.packname = 'aylize bot whatsapp' // nama pack sticker
+global.namabot = 'AYLIZE BOTZ' // nama bot mu
+global.group = 'https://watashianime.my.id' // grup mu
+global.pic = 'https://telegra.ph/file/3d3e73c4cb0682671f25a.jpg' // logo bot
 
 
 
